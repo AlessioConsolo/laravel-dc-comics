bf946be01387e153545ac63ad318a905ddcda6eb
AlessioConsolo <alessioconsolo3@gmail.com>
2024-09-29 22:29:47 +0200

    [UPD] Added comics table and comicsSeeder

--- database/migrations/2024_09_29_183451_create_comics_table.php
+++ database/migrations/2024_09_29_183451_create_comics_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateComicsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('thumb');
+            $table->float('price', 8, 2);
+            $table->string('series');
+            $table->date('sale_date');
+            $table->string('type');
+            $table->string('artists');
+            $table->string('writers');
             $table->timestamps();
         });
     }
@@ -28,4 +37,4 @@ return new class extends Migration
     {
         Schema::dropIfExists('comics');
     }
-};
+}
