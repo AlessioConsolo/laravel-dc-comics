162d7d2ecf8497e73a6276fc20149e0e2669d481
AlessioConsolo <alessioconsolo3@gmail.com>
2024-09-29 22:43:38 +0200

    [UPD] Added route to web.php

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use App\Http\Controllers\ComicController;
 |
 */
 
+Route::get('/', function () {
+    return redirect()->route('comics.index');
+});
 
 Route::resource('comics', ComicController::class);
