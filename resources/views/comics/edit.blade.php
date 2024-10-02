b41d379180ac5eb3d337287cfb9b2942a904cf88
AlessioConsolo <alessioconsolo3@gmail.com>
2024-10-02 23:22:25 +0200

    [UPD] added an "if" on edit.blade to show errors

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -3,51 +3,81 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Modifica Fumetto</title>
+    <title>Aggiungi un nuovo fumetto</title>
 </head>
 <body>
-    <h1>Modifica Fumetto: {{ $comic->title }}</h1>
+    <h1>Aggiungi un nuovo fumetto</h1>
 
-    <form action="{{ route('comics.update', $comic->id) }}" method="POST">
+    @if ($errors->any())
+        <div style="color: red;">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <form action="{{ route('comics.store') }}" method="POST">
         @csrf
-        @method('PUT')
 
         <div>
             <label for="title">Titolo:</label>
-            <input type="text" id="title" name="title" value="{{ $comic->title }}" required>
+            <input type="text" id="title" name="title" value="{{ old('title') }}" required>
+            @error('title')
+                <div style="color: red;">{{ $message }}</div>
+            @enderror
         </div>
 
         <div>
             <label for="description">Descrizione:</label>
-            <textarea id="description" name="description" required>{{ $comic->description }}</textarea>
+            <textarea id="description" name="description" required>{{ old('description') }}</textarea>
+            @error('description')
+                <div style="color: red;">{{ $message }}</div>
+            @enderror
         </div>
 
         <div>
             <label for="thumb">URL Copertina:</label>
-            <input type="text" id="thumb" name="thumb" value="{{ $comic->thumb }}" required>
+            <input type="text" id="thumb" name="thumb" value="{{ old('thumb') }}" required>
+            @error('thumb')
+                <div style="color: red;">{{ $message }}</div>
+            @enderror
         </div>
 
         <div>
             <label for="price">Prezzo:</label>
-            <input type="number" step="0.01" id="price" name="price" value="{{ $comic->price }}" required>
+            <input type="number" step="0.01" id="price" name="price" value="{{ old('price') }}" required>
+            @error('price')
+                <div style="color: red;">{{ $message }}</div>
+            @enderror
         </div>
 
         <div>
             <label for="series">Serie:</label>
-            <input type="text" id="series" name="series" value="{{ $comic->series }}" required>
+            <input type="text" id="series" name="series" value="{{ old('series') }}" required>
+            @error('series')
+                <div style="color: red;">{{ $message }}</div>
+            @enderror
         </div>
 
         <div>
             <label for="sale_date">Data di uscita:</label>
-            <input type="date" id="sale_date" name="sale_date" value="{{ $comic->sale_date }}" required>
+            <input type="date" id="sale_date" name="sale_date" value="{{ old('sale_date') }}" required>
+            @error('sale_date')
+                <div style="color: red;">{{ $message }}</div>
+            @enderror
         </div>
 
         <div>
             <label for="type">Tipo:</label>
-            <input type="text" id="type" name="type" value="{{ $comic->type }}" required>
+            <input type="text" id="type" name="type" value="{{ old('type') }}" required>
+            @error('type')
+                <div style="color: red;">{{ $message }}</div>
+            @enderror
         </div>
 
-        <button type="submit">Aggiorna Fumetto</button>
+        <button type="submit">Salva Fumetto</button>
     </form>
 
     <a href="{{ route('comics.index') }}">Torna alla lista dei fumetti</a>
